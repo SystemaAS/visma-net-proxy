05ce63a43a2dacc3ed60d0a9bf7538c2151b388e
Fredrik Möller <fredrik@systema.no>
2018-04-12 10:40:11 +0200

    Draft

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # visma-net-proxy
 The Visma.net proxy
+
+## Generate
+
+		swagger-codegen generate  \
+		     -i https://integration.visma.net/API-index/doc/swagger \
+		     -l java \
+		     -c /<path>/swagger-java-config.json \
+		     -o /<path>
+ 
+     
+Example:
+
+		swagger-codegen generate  \
+		     -i https://integration.visma.net/API-index/doc/swagger \
+		     -l java \
+		     -c /Users/fredrikmoller/git/visma-net-proxy/swagger-java-config.json \
+		     -o /Users/fredrikmoller/Temp/VismaNetAPI
--- src/no/systema/visma/HelperController.java
+++ src/no/systema/visma/HelperController.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpSession;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +19,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import no.systema.jservices.common.dao.services.BridfDaoService;
 import no.systema.visma.integration.TransactionManager;
+import no.systema.visma.v1client.ApiClient;
+import no.systema.visma.v1client.api.CustomerApi;
+import no.systema.visma.v1client.model.CustomerDto;
 /**
  * 
  * 
@@ -24,6 +29,9 @@ import no.systema.visma.integration.TransactionManager;
  * @date 2018-03
  *
  */
+
+@Configuration
+@ComponentScan(basePackages = {"no.systema", "org.springframework.web.client"})
 @Controller
 public class HelperController {
 	private static Logger logger = Logger.getLogger(HelperController.class.getSimpleName());
@@ -41,18 +49,37 @@ public class HelperController {
 		StringBuilder sb = new StringBuilder();
 //		List<PrettyPrintAttachments> dagsoppgors = null;
 
-		logger.info("syncronizeCustomers.do...");
+		logger.info("getCustomer.do...");
 		try {
 			String user = request.getParameter("user");
-//		(user, "user must be delivered."); 
+			
+			logger.info("bridfDaoService="+bridfDaoService);
 			
 			String userName = bridfDaoService.getUserName(user);
-//			Assert.notNull(userName, "userName not found in Bridf."); 
 			
-			transactionManager.syncronizeCustomers();
+			String customerCd = request.getParameter("id");
+
+//			List<CustomerDto> customerList = null; 
+//			customerList = client.getCustomer().getCustomerGetAll(null, null, null, null, null, null, null, null, null, null, null);
+//			assertNotNull("customerList should not be null", customerList);
+			
+//			customerList.forEach((dto) ->System.out.println(ReflectionToStringBuilder.toString(dto)));
+			
+//			final ApiClient apiClient = new ApiClient(); //Using default BufferingClientHttpRequestFactory
+			
+			apiClient.setBasePath("https://integration.visma.net/API");
+			apiClient.addDefaultHeader("ipp-application-type", "Visma.net Financials");
+			apiClient.addDefaultHeader("ipp-company-id", "1684147");
+			apiClient.setAccessToken("81d21509-a23e-40a3-82d4-b101bb681d0f");
+			
+			
+//			final CustomerApi api = new CustomerApi(apiClient);
+			
+	        CustomerDto response = api.customerGetBycustomerCd(customerCd);
+			
+			
 			sb.append("nån skön text \n \n");
 			
-//			sb.append(FlipTableConverters.fromIterable(dagsoppgors, PrettyPrintAttachments.class));
 			
 		} catch (Exception e) {
 			// write std.output error output
@@ -104,11 +131,20 @@ public class HelperController {
 	}
 
 	@Autowired
+	@Qualifier("bridfDaoService")
 	private BridfDaoService bridfDaoService;
 	
 	@Autowired
 	@Qualifier("tsManager")
 	private TransactionManager transactionManager;
+	
+	@Autowired
+	@Qualifier("no.systema.visma.v1client.ApiClient")
+	private ApiClient apiClient;
+	
+	@Autowired
+	@Qualifier("no.systema.visma.v1client.api.CustomerApi")
+	public CustomerApi api = new CustomerApi(apiClient);
 	
 	
 }
